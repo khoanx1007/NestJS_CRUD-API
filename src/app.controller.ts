557975f1780d55921f0060f2c99cc2b84a5cdd33
khoanx1007 <khoanx1007@gmail.com>
2024-07-09 13:34:06 +0700

    pipes example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { FreezePipe } from './pipes/freeze.pipe';
 
 @Controller()
 export class AppController {
@@ -10,4 +11,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Post()
+  examplePipe(@Body(new FreezePipe()) body: any){
+    body.test = 123;
+  };
 }
